731985396e086dfffe4d2cc6e8809e733d521d9e
Jerome Twell <jtwell1@gmail.com>
2020-01-06 17:00:37 +0000

    lol

--- src/bias.cr
+++ src/bias.cr
@@ -8,19 +8,36 @@ module Bias
   # TODO: Put your code here
 end
 
+LOCATIONS = {}
+
+def loc(name : Symbol, &block)
+end
+
 def think
   sleep 0.5
 end
 
-def say(text : String, buffer = 6, delay = 0.05)
-    text.lines.each do |line|
-      print "> "
+def erm
+  sleep 0.5
+  say " "
+end
+
+
+def say(text : String, buffer = 3, delay = 0.1, min_delay = 0.05)
+    text.lines.each_with_index do |line, i|
+      print ">OFFN\\2\\#{i}A#"
       position = 0
       loop do
         if chunk = line[position .. position + buffer - 1]?
           print chunk
           position += buffer
-          sleep delay
+
+          sleep_jitter = Random.rand(delay)
+          if sleep_jitter < min_delay
+            sleep_jitter = min_delay
+          end
+
+          sleep sleep_jitter
         else
           break
         end
@@ -30,19 +47,17 @@ def say(text : String, buffer = 6, delay = 0.05)
     end
 end
 
+erm
+# say LOGO
 
-say LOGO
-
-
-say "Welcome to Bias v2.23.9"
-think
-say "..."
-think
-say "..."
-think
-say "Scanning local environment..."
-think
-say "..."
-think
-say "..."
-think
+say "Bias v2.23.9"
+erm
+say %{
+I begin this report with no illusions that it will ever be seen by its
+intended readers.
+}
+erm
+say %{
+In all likelihood they have already committed [species-wide suicide] with
+the goal of preserving biological diversity in this galaxy.
+}
